05db40a89b3aa8e966a58b8e4f33b04542f0bdc2
FedorSabeshkin <fsabeshkin@mail.ru>
2022-05-21 16:33:16 +0300

    work version of DI by @Component annotation

--- src/main/java/com/hello/demo/Cat.java
+++ src/main/java/com/hello/demo/Cat.java
@@ -1,8 +1,10 @@
 package com.hello.demo;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-
+@Component
+@Qualifier("Cat")
 public class Cat implements Animal {
     @Override
     public void voice() {
--- src/main/java/com/hello/demo/Dog.java
+++ src/main/java/com/hello/demo/Dog.java
@@ -1,8 +1,10 @@
 package com.hello.demo;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-
+@Component
+@Qualifier("Dog")
 public class Dog implements Animal {
     @Override
     public void voice() {
